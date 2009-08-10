ac8bb685233be684704c62b752635fe34be0c32a
ronkeizer <ronkeizer@8dbea5f8-3d6b-11de-b438-597f59cd7555>
2009-08-10 13:00:32 +0000

    Test vanaf SLZ

--- subs.pl
+++ subs.pl
@@ -1,3 +1,24 @@
+#    ----------------------------------------------------------------------
+#    Piraña
+#    Copyright Ron Keizer, 2007-2009, Amsterdam, the Netherlands
+#    ----------------------------------------------------------------------
+#
+#    This file is part of Piraña.
+#
+#    Piraña is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.                                  
+#
+#    Piraña is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Piraña.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 # Subroutines for pirana
 # These are mainly the subs that build parts of the GUI and dialogs.
 # As much as possible, subs are located in separate modules
@@ -5,6 +26,7 @@
 sub get_psn_nm_versions {
 ### Purpose : Retrieve the NM versions specified to psn. (reads a pipe from "psn -nm_versions") 
 ### Compat  : W+L+
+### Notes   : When the psn command is invoked but cannot be found, Pirana crashes
   my @split;
   our $max_psn_name;
   my %psn_nm_versions; my %psn_nm_versions_vers;
@@ -4242,8 +4264,7 @@ sub show_run_method {
 
 sub method_specific_options {
 ### Purpose : Show the buttons for "NONMEM", "PsN" or "WFN" run methods 
-### Compat  : W+L-
-### Notes   : Makes the *nix version crash...
+### Compat  : W+L+
   if ($run_method eq "") {$run_method = "PsN";};
   if ($local_nm) {$local_nm-> gridForget()};
   if ($method_options) {$method_options-> gridForget()};
