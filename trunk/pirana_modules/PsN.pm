8ea57fad5e6020bb5f48f630d73f5f4c07380ca6
ronkeizer <ronkeizer@8dbea5f8-3d6b-11de-b438-597f59cd7555>
2009-08-12 08:54:52 +0000

    Corrected cluster activation problem

--- trunk/pirana_modules/PsN.pm
+++ trunk/pirana_modules/PsN.pm
@@ -5,7 +5,7 @@ package pirana_modules::PsN;
 use strict;
 require Exporter;
 our @ISA = qw(Exporter);
-our @EXPORT_OK = qw(get_psn_info get_psn_help);
+our @EXPORT_OK = qw(get_psn_info get_psn_help get_psn_nm_versions);
 
 sub get_psn_info {
 ### Purpose : Get the info on a PsN command, by invoking the -h switch and capturing the output 
@@ -37,4 +37,47 @@ sub get_psn_help {
     message ("PsN help file for command ".$psn_command." not found");
   }
 }
+
+sub get_psn_nm_versions {
+### Purpose : Retrieve the NM versions specified to psn. (reads a pipe from "psn -nm_versions") 
+### Compat  : W+L+
+### Notes   : When the psn command is invoked but cannot be found, Pirana crashes
+  my ($setting_ref, $software_ref, $cluster_active)= @_;
+  my %setting = %$setting_ref;
+  my %software = %$software_ref; 
+  my @split;
+  our $max_psn_name;
+  my %psn_nm_versions; my %psn_nm_versions_vers;
+  if ($software{psn_dir} =~ m/perl/i) {  # use the PsN that is specified, not necisssarily the one in the system variables 
+    my $psn_dir = $software{psn_dir};
+    @split = split(/\\/,$psn_dir); 
+  }
+  my $command;
+  unless (($setting{use_cluster}==1)&&($cluster_active==1)) {
+    if (-e @split[0]."\\".@split[1]."\\bin\\psn") {
+      $command = @split[0]."\\".@split[1]."\\bin\\psn -nm_versions";
+    } else {$command = "psn -nm_versions";}
+  } else {  # on cluster using SSH
+      $command = $setting{ssh_login}.' '.$software{psn_on_cluster}.'"psn -nm_versions &"';
+  }
+  if (my $stdout) {my $stdout -> insert('end', "\n".$command);};
+  open (OUT, $command." |") or die "Could not open command: $!\nCheck installation of PsN.";
+  my $flag = 0;
+  my %psn_nm_versions;
+  while (my $line = <OUT>) {
+     if ($line =~ m/will call/gi) {
+       my ($nm_name, $nm_loc) = split(/\(will call /,$line);
+       my ($nm_loc, $nm_ver) = split (/,/, $nm_loc);
+       $nm_name =~ s/\s//g;
+       chomp($nm_ver); $nm_ver =~ s/\)//;
+     #  $nm_name = substr($nm_name,0,9);
+       if(length($nm_name)>$max_psn_name) {$max_psn_name=length($nm_name)}
+       $psn_nm_versions{$nm_name} = $nm_loc;
+       $psn_nm_versions_vers{$nm_name} = $nm_ver;
+     }
+  }
+  if ($max_psn_name<9) {$max_psn_name=9};
+  return \%psn_nm_versions, \%psn_nm_versions_vers;  
+}
 1;
+
